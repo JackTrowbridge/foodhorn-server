a3c1dd362eb0424858fed6103c6522241a1e6f63
Jack Trowbridge <jacktrowbridge2004@gmail.com>
2024-02-04 14:31:27 +0000

    Implement functions for database restructure

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import firebase_admin
 from firebase_admin import credentials, auth, firestore, initialize_app, storage
 from urllib.parse import unquote
+from random_username.generate import generate_username
 
 cred = credentials.Certificate("firebase-key.json")
 firebase_admin.initialize_app(cred, {
@@ -24,12 +25,10 @@ def verify_token():
     except Exception as e:
         return jsonify({'error': str(e)}), 401
 
-
-@app.route('/addPostToUser', methods=['POST'])
-def add_post_to_user():
+@app.route('/getUserData', methods=['POST'])
+def get_user_data():
     data = request.json
     id_token = data.get('idToken')
-    post_id = data.get('postId')
 
     try:
         # Verify the ID token and get the UID of the user
@@ -38,6 +37,140 @@ def add_post_to_user():
 
         # Initialize Firestore
         db = firestore.client()
+
+        # Reference to the user's document
+        user_ref = db.collection('users').document(uid)
+
+        # Get the user's document
+        doc = user_ref.get()
+
+        if doc.exists:
+            # If the document exists, return the user's data
+            return jsonify({
+                'message': 'User data created successfully',
+                'username': doc.to_dict()['username'],
+                'posts': get_users_posts(uid)
+                }), 200
+        else:
+            # If the document does not exist, create one and set defaults
+
+            username = generate_username(1)[0]
+
+            user_ref.set({'username': username})
+            return jsonify({
+                'message': 'User data created successfully',
+                'username': username,
+                }), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/updateUserData', methods=['POST'])
+def update_user_data():
+    data = request.json
+    id_token = data.get('idToken')
+    username = data.get('username')
+
+    try:
+        # Verify the ID token and get the UID of the user
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']
+
+        # Initialize Firestore
+        db = firestore.client()
+
+        # Reference to the user's document
+        user_ref = db.collection('users').document(uid)
+
+        print("Username: ", username)
+
+        # Update the user's document with the new data
+        user_ref.update({
+            'username': username
+        })
+
+        return jsonify({
+            'message': 'User data updated successfully',
+            'username': username
+        }), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 401
+
+@app.route('/deletePost', methods=['POST'])
+def delete_post():
+    data = request.json
+    id_token = data.get('idToken')
+    post_id = data.get('postId')
+
+    try:
+
+        # Verify the ID token and get the UID of the user
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']
+
+        # Initialize Firestore
+        db = firestore.client()
+
+        # Reference to the user's document
+        user_ref = db.collection('users').document(uid)
+        post_ref = db.collection('posts').document(post_id)
+
+        for post in user_ref.get().to_dict()['posts']:
+            if post == post_id:
+                user_ref.update({
+                    'posts': firestore.ArrayRemove([post_id])
+                })
+
+                content_url = post_ref.get().to_dict()['content_url']
+                thumbnail_url = post_ref.get().to_dict()['thumbnail_url']
+
+                if content_url:
+                    file_path = unquote(content_url.split('/o/', 1)[-1].split('?')[0])
+                    file_path = file_path.replace('All%20Videos/', 'All Videos/')
+                    bucket = storage.bucket()
+                    blob = bucket.blob(file_path)
+                    blob.delete()
+                
+                if thumbnail_url:
+                    thumbnail_path = unquote(thumbnail_url.split('/o/', 1)[-1].split('?')[0])
+                    thumbnail_path = thumbnail_path.replace('All%20Thumbnails/', 'All Thumbnails/')
+                    thumbnail_blob = bucket.blob(thumbnail_path)
+                    thumbnail_blob.delete()
+
+                post_ref.delete()
+                return jsonify({'message': 'Post removed from user successfully'}), 200
+
+        return jsonify({'error': 'Post not found or unauthorized access'}), 404
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 401
+
+@app.route('/addPost', methods=['POST'])
+def add_post_to_user():
+    data = request.json
+    id_token = data.get('idToken')
+    post_data = data.get('post')
+
+    try:
+        # Verify the ID token and get the UID of the user
+        decoded_token = auth.verify_id_token(id_token)
+        uid = decoded_token['uid']
+
+        # Initialize Firestore
+        db = firestore.client()
+
+        # Create a new document reference with an auto-generated ID in the 'posts' collection
+        post_ref = db.collection('posts').document()
+        post_id = post_ref.id  # Get the auto-generated ID
+
+
+        # Set the post data to the new document
+        post_ref.set(post_data)
+
+        post_ref.update({
+            'post_id': post_id,
+        })
 
         # Reference to the user's document
         user_ref = db.collection('users').document(uid)
@@ -65,89 +198,22 @@ def add_post_to_user():
     except Exception as e:
         return jsonify({'error': str(e)}), 401
 
-def remove_post_from_user(uid, post_id):
+def get_users_posts(uid):
 
-    try:
-        # Initialize Firestore
-        db = firestore.client()
+    posts = []
 
-        # Reference to the user's document
-        user_ref = db.collection('users').document(uid)
+    # Initialize Firestore
+    db = firestore.client()
 
-        # Run a transaction to ensure atomicity
-        @firestore.transactional
-        def update_user(transaction, user_ref, post_id):
-            snapshot = user_ref.get(transaction=transaction)
-            if snapshot.exists:
-                transaction.update(user_ref, {
-                    'posts': firestore.ArrayRemove([post_id])
-                })
+    # Reference to the user's document
+    user_ref = db.collection('users').document(uid)
+    # get lists in posts
+    post_list = user_ref.get().to_dict()['posts']
+    for post in post_list:
+        post_ref = db.collection('posts').document(post)
+        posts.append(post_ref.get().to_dict())
 
-        # Start the transaction
-        transaction = db.transaction()
-        update_user(transaction, user_ref, post_id)
-
-        return jsonify({'message': 'Post removed from user successfully'}), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 401
-
-@app.route('/deletePost', methods=['POST'])
-def delete_post():
-    data = request.json
-    id_token = data.get('idToken')
-    post_id = data.get('postId')
-
-    try:
-        # Verify the ID token and get the UID of the user
-        decoded_token = auth.verify_id_token(id_token)
-        uid = decoded_token['uid']
-
-        # Initialize Firestore
-        db = firestore.client()
-
-        # Query for the specific post using the post_id field
-        posts = db.collection('posts').stream()
-
-        post_found = False
-        for post in posts:
-            post_data = post.to_dict()
-            if post_data.get('post_id') == post_id and post_data.get('creator_id') == uid:
-                # Before deleting the post, delete the file from Firebase Storage
-
-                content_url = post_data.get('content_url')
-                if content_url:
-                    # Extract the file path from the content_url
-                    file_path = unquote(content_url.split('/o/', 1)[-1].split('?')[0])
-                    file_path = file_path.replace('All%20Videos/', 'All Videos/')  # Handle URL encoding
-                    # Initialize Firebase Storage
-                    bucket = storage.bucket()
-                    blob = bucket.blob(file_path)
-                    blob.delete()
-
-                # Delete the thumbnail file from Firebase Storage
-                thumbnail_url = post_data.get('thumbnail_url')
-                if thumbnail_url:
-                    thumbnail_path = unquote(thumbnail_url.split('/o/', 1)[-1].split('?')[0])
-                    thumbnail_path = thumbnail_path.replace('All%20Thumbnails/', 'All Thumbnails/')  # Correct directory name
-                    thumbnail_blob = bucket.blob(thumbnail_path)
-                    thumbnail_blob.delete()
-
-                # Remove the post_id from the user's document
-                remove_post_from_user(uid, post_id)
-
-                # Now delete the Firestore document
-                post.reference.delete()
-                post_found = True
-                break
-
-        if not post_found:
-            return jsonify({'error': 'Post not found or unauthorized access'}), 404
-
-        return jsonify({'message': 'Post deleted successfully'}), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 401
+    return posts
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5200)
